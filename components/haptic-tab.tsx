77274ed9ac439c5865803218c4da779439f1457b
zubbicodes <zubbihawk@proton.me>
2026-01-19 19:53:34 +0500

    Sound Effect - UI Fix - Happtic Feedback

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,15 +1,16 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
+import { useSettings } from '@/context/SettingsContext';
 import * as Haptics from 'expo-haptics';
 
 export function HapticTab(props: BottomTabBarButtonProps) {
+  const { hapticsEnabled } = useSettings();
   return (
     <PlatformPressable
       {...props}
       onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        if (hapticsEnabled && process.env.EXPO_OS === 'ios') {
+          void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         }
         props.onPressIn?.(ev);
       }}
